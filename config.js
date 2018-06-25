42e6e604c501b36b90b72dd3945948ee0cf59aba
williampayne23 <williampayne23@gmail.com>
2018-06-25 15:17:03 +0100

    Added a few options to config file

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  email: "FB_EMAIL",
-  password: "FB_PASSWORD"
+  credentials : {
+    email: "FB_EMAIL",
+    password: "FB_PASSWORD"
+  },
+  bot_user_id : 'BOTS FACEBOOK USER ID',
+  logfile : '.log.html',
+  saved_messages_file : 'data/saved_messages.json',
+  saved_data_file : 'data/data.json'
 }
